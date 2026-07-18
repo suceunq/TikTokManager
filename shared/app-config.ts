9881e33c685a2341cf2071f4af911e3bc3bdca5e
suceunq <bob62138@gmail.com>
2026-07-18 17:36:16 +0200

    Corriger l'identifiant PayPal

--- shared/app-config.ts
+++ shared/app-config.ts
@@ -1,3 +1,3 @@
 /** Official donation page opened by the main process. It is never exposed as a user setting. */
 export const DEFAULT_DONATION_URL =
-  'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=X6TNHGN5K7QA&item_name=Soutenir%20le%20developpement%20de%20TikTok%20Manager&currency_code=EUR';
+  'https://www.paypal.com/donate/?business=X65TNHGN5K7QA&no_recurring=0&item_name=TikTok%20Manager&currency_code=EUR';
